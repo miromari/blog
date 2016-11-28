51849b969d87a802dd4e58abba3048458cf2ae22
maria <marg13@yandex.ru>
2016-11-28 12:19:58 +0300

    Поменяла функцию autoload на предложенную Дмитрием http://www.php-fig.org/psr/psr-0/#example-implementation

--- index.php
+++ index.php
@@ -1,13 +1,32 @@
 <?php
 
 include_once 'settings.php';
-function __autoload($classname)
+// function __autoload($classname)
+// {
+//     include_once str_replace("\\", DIRECTORY_SEPARATOR, $classname) . '.php';
+// }
+
+
+
+function autoload($className)
 {
-    include_once str_replace("\\", DIRECTORY_SEPARATOR, $classname) . '.php';
+    $className = ltrim($className, '\\');
+    $fileName  = '';
+    $namespace = '';
+    if ($lastNsPos = strrpos($className, '\\')) {
+        $namespace = substr($className, 0, $lastNsPos);
+        $className = substr($className, $lastNsPos + 1);
+        $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+    }
+    $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
+
+    require $fileName;
 }
+spl_autoload_register('autoload');
 
 session_start();
 
 $app = new Core\App(new Core\Request($_GET,$_POST,$_SERVER));
 
 $app->go();  
+
